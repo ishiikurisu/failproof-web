be353c823a7e20cbff8938118a6ac4e38cb31dc7
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-06-02 16:00:38 -0300

    Learning how to use the templating system

--- src/clj/br/eng/crisjr/failproof/server/routes/home.clj
+++ src/clj/br/eng/crisjr/failproof/server/routes/home.clj
@@ -5,13 +5,16 @@
             [clojure.java.io :as io]))
 
 (defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+  (layout/render "home.html" ))
 
 (defn about-page []
   (layout/render
     "about.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
+(defn checklists-page []
+  (layout/render "checklists.html" {:titles ["no such thing" "indeed"]}))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/checklists" [] (checklists-page)))
