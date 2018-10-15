d4f79d0080021fabc2bbb47d6995c988c39bcc1d
Cristiano Alves da Silva Junior <cristiano.junior@mbra.com.br>
2018-10-15 12:22:56 -0300

    Adding example checklists to cookies

--- js/checklists.js
+++ js/checklists.js
@@ -1,9 +1,50 @@
+function addChecklistWithIndex(index) {
+    $.get("/stuff/examples.json", function(checklists) {
+        addChecklist(checklists[index]);
+        window.location = "/";
+    });
+}
+
+function createCardForChecklist(index, checklists) {
+    var checklist = checklists[index];
+    var col = $("<div/>", { class: "col-md-4" });
+    var card = $("<div/>", { class: "card mb-4" });
+    var cardHead = $("<div/>", { class: "card-head" });
+    var cardHeader = $("<h3/>", { text: checklist.title });
+    var cardBody = $("<div/>", { class: "card-body" });
+    var checklistItems = $("<ul/>");
+    var addChecklistButton = $("<button/>", { 
+        class: "btn btn-sm btn-outline-primary",
+        type: "button",
+        text: "Add me!",
+        id: "button-add-" + index,
+        onclick: "addChecklistWithIndex(" + index + ")"
+    });
+
+    for (var i = 0; i < checklist.items.length; i++) {
+        $("<li/>", { html: checklist.items[i].title }).appendTo(checklistItems);
+    }
+
+    cardBody.append(checklistItems);
+    cardBody.append(addChecklistButton);
+    cardHead.append(cardHeader);
+    card.append(cardHead);
+    card.append(cardBody);
+    col.append(card);
+
+    return col;
+}
+
 $(document).ready(function() {
     // Drawing example checklists
     $.get("/stuff/examples.json", function(checklists) {
+        var row = $("<div/>", { class: "row" });
         for (var i = 0; i < checklists.length; i++) {
             var checklist = checklists[i];
-            // TODO Add checklist to page
+            var card = createCardForChecklist(i, checklists);
+            var cardId = "#button-add-" + i;
+            row.append(card);
         }
+        $("#content").html(row);
     });
 });
--- js/failproof/checklists.js
+++ js/failproof/checklists.js
@@ -5,6 +5,7 @@ function getChecklists() {
     var checklists = [];
     var raw = getCookie('checklists');
 
+    
     if (raw.length > 0) {
         checklists = JSON.parse(raw);
     }
@@ -13,16 +14,23 @@ function getChecklists() {
 }
 
 /**
+ * Adds a checklist to the memory.
+ */
+function addChecklist(checklist) {
+    setCookie('checklists', JSON.stringify(getChecklists().concat(checklist)));
+}
+
+/**
  * Turns a checklist object into a string.
  */
 function checklistToString(raw) {
-    var outlet = raw['title'] + '\n';
-    var items = raw['items'];
+    var outlet = raw.title + '\n';
+    var items = raw.items;
 
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
-        outlet += (item['checked'])? '[-] ' : '[ ] ';
-        outlet += item['title'] + '\n';
+        outlet += (item.checked)? '[-] ' : '[ ] ';
+        outlet += item.title + '\n';
     }
 
     return outlet;
