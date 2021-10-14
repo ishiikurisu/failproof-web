551345468ea1882a6cf65d3b1d1c36571582137d
Cris Jr <crisjr@pm.me>
2021-10-14 15:27:27 -0300

    Create and list new notes

---
+++ js/view/index.js
@@ -0,0 +1,41 @@
+function generateNoteIdList() {
+    var noteIndex = getNotes();
+    var outlet = `
+        <p>
+            Start by creating a new note
+        </p>
+    `;
+
+    if (noteIndex.length > 0) {
+        outlet = `<ul>`;
+        for (var i = 0; i < noteIndex.length; i++) {
+            var noteId = noteIndex[i];
+            var note = getNote(noteId);
+            outlet += `
+                <li>
+                    <a href="note.html?id=${noteId}">${note.title}</a>
+                </li>
+            `;
+        }
+        outlet += `</ul>`
+    }
+
+    return outlet;
+}
+
+function newNoteButtonClick() {
+    var id = createNote();
+    window.location.href = window.location.origin + `/note.html?id=${id}`
+}
+
+function setup() {
+    initDb();
+
+    // note list setup
+    var content = document.getElementById("content");
+    content.innerHTML = generateNoteIdList();
+
+    // new button setup
+    var newNoteButton = document.getElementById("new-note-button");
+    newNoteButton.addEventListener("click", newNoteButtonClick);
+}
---
+++ js/view/note.js
@@ -0,0 +1,20 @@
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+function setup() {
+    var noteId = findGetParameter("id");
+    var note = getNote(noteId);
+
+    document.getElementById("title").innerHTML = note.title;
+    document.getElementById("contents").innerHTML = note.contents;
+}
